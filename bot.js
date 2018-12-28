0c23c39a7d541fcd3fc7afb5729dde61ce596744
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-12-28 18:14:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,6 +56,7 @@ client.on('message', (message) => {
         if (message.content === '.stop') {stop.push(message.guild.id); return message.channel.send('Offline');}
         if (message.content === '.start') {stop.splice(stop.indexOf(message.guild.id),1); return message.channel.send('Online');}
         if (message.content === '.invite') {; return message.channel.send('https://discordapp.com/oauth2/authorize?client_id=528186840822579230&permissions=2146958833&scope=bot');}
+        if (message.content === '.support') {; return message.channel.send('https://discord.gg/XjA5Ven');}
     }  
 })
 client.login(process.env.TOKEN);
